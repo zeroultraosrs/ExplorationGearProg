717984c7a6b1db68214fd697b7eb24018cf8a2af
Mads <anderen72@gmail.com>
2024-04-26 14:01:56 +0200

    test

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var imagesContainer = document.querySelector(".flex-container");
 // Add click event listener to the parent element
 imagesContainer.addEventListener("click", function (event) {
     // Check if the clicked element is an image
+    console.log("Clicked element:", event.target);
     if (event.target.tagName === "IMG") {
         // Toggle the "green-background" class on the clicked image
         event.target.classList.toggle("green-background");
     }
-    if (event.target.tagName === "SKILL") {
+    if (event.target.tagName === "SKILL" || EventSource.target.classList.contains("skill")) {
         // Toggle the "green-background" class on the clicked image
         event.target.classList.toggle("green-background");
     }
